080d698767a6d345bc9448310dac46fb7b390733
ivannpaz <ivannpaz@gmail.com>
2017-01-09 20:08:27 +0100

    Basic RootEndpoint setup

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Print("'Hola'")
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", RootEndpoint).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":1337", router))
+}
+
+// RootEndpoint ...
+//
+func RootEndpoint(w http.ResponseWriter, req *http.Request) {
+	var payload [3]string
+
+	payload[0] = "1"
+	payload[1] = "2"
+	payload[2] = "3"
+
+	json.NewEncoder(w).Encode(payload)
 }
